5e135c45c526ae4f8f849d6c0a2f108bb019b6d0
Vivek Kalyan <hello@vivekkalyan.com>
2025-02-19 17:49:08 +0800

    feat: Create index command that runs graph binary

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,96 @@
-console.log("Hello via Bun!");
+import { exec } from "node:child_process";
+import { join } from "node:path";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { CallToolRequestSchema, ListToolsRequestSchema, ToolSchema } from "@modelcontextprotocol/sdk/types.js";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+const server = new Server(
+  {
+    name: "mcp-server-codegraph",
+    version: "0.1.0",
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  },
+);
+
+const IndexArgsSchema = z.object({
+  path: z.string().describe("Directory of codebase to index"),
+});
+
+const ToolInputSchema = ToolSchema.shape.inputSchema;
+type ToolInput = z.infer<typeof ToolInputSchema>;
+
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+  return {
+    tools: [
+      {
+        name: "index",
+        description: "Index the codebase",
+        inputSchema: zodToJsonSchema(IndexArgsSchema) as ToolInput,
+      },
+    ],
+  };
+});
+
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  try {
+    const { name, arguments: args } = request.params;
+
+    switch (name) {
+      case "index": {
+        const parsedArgs = IndexArgsSchema.safeParse(args);
+        if (!parsedArgs.success) {
+          throw new Error(`Invalid arguments for index: ${parsedArgs.error}`);
+        }
+
+        const { stdout, stderr } = await new Promise<{
+          stdout: string;
+          stderr: string;
+        }>((resolve, reject) => {
+          exec(
+            `./graph ${parsedArgs.data.path} ${join(parsedArgs.data.path, "index.json")}`,
+            (error, stdout, stderr) => {
+              if (error) {
+                reject({ error, stdout, stderr });
+              } else {
+                resolve({ stdout, stderr });
+              }
+            },
+          );
+        });
+
+        return {
+          content: [{ type: "text", text: `stdout: ${stdout}\nstderr: ${stderr}` }],
+        };
+      }
+      default:
+        return {
+          content: [{ type: "text", text: `Unknown tool: ${name}` }],
+          isError: true,
+        };
+    }
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    return {
+      content: [{ type: "text", text: `Error: ${errorMessage}` }],
+      isError: true,
+    };
+  }
+});
+
+// Start server
+async function runServer() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("MCP Server CodeGraph running on stdio");
+}
+
+runServer().catch((error) => {
+  console.error("Fatal error running server:", error);
+  process.exit(1);
+});
